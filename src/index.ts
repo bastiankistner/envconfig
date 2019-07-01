ff2c3558c44c2696d8def55c1ba1f14c182ccc8e
Bastian Kistner <bastian.kistner@gmail.com>
2019-07-01 15:12:57 +0200

    changed error handling

--- src/index.ts
+++ src/index.ts
@@ -23,6 +23,11 @@ export interface Specification {
     | null;
 }
 
+function handleError(message: string) {
+  console.error(message);
+  throw new Error(message);
+}
+
 export type Config = {
   [key: string]: any;
 };
@@ -32,11 +37,11 @@ export const describe = <T extends { [key: string]: any }>(
   input: { [key: string]: any } = process.env
 ): T => {
   if (typeof specification !== "object") {
-    throw new Error("The first argument must be an object");
+    handleError('The first argument must be an object');
   }
 
   if (typeof input !== "object") {
-    throw new Error("The second argument must be an object");
+    handleError('The second argument must be an object');
   }
 
   return Object.keys(specification).reduce<T>(
@@ -57,21 +62,19 @@ export const describe = <T extends { [key: string]: any }>(
       let value = input[itemSpecification.name || key];
 
       if (!type && !rest.sanitize) {
-        throw new Error(`Invalid specification: either ${key}.type or ${key}.sanitize is required`);
+        handleError(`Invalid specification: either ${key}.type or ${key}.sanitize is required`);
       }
 
       if (type) {
         if (!sanitizers[type as Type]) {
-          throw new Error(
-            `Invalid specification: ${key}.type is invalid (valid types are: ${Object.keys(sanitizers).join(", ")}`
-          );
+          handleError(`Invalid specification: ${key}.type is invalid (valid types are: ${Object.keys(sanitizers).join(', ')}`);
         }
 
         const isStandardDefined = typeof rest.default !== "undefined";
         const wasInitiallyDefined = typeof value !== "undefined";
 
         if (!rest.isOptional && !wasInitiallyDefined && !isStandardDefined) {
-          throw new Error(`Required: ${key}`);
+          handleError(`Required: ${key}`);
         }
 
         if (wasInitiallyDefined) {
@@ -85,17 +88,19 @@ export const describe = <T extends { [key: string]: any }>(
         const item = itemSpecification;
 
         if (typeof item.sanitize !== "function") {
-          throw new Error(`Invalid specification: ${key}.sanitize must be a function`);
+          handleError(`Invalid specification: ${key}.sanitize must be a function`);
+        } else {
+          value = item.sanitize(value);
         }
 
-        value = item.sanitize(value);
       }
 
       if (typeof value === "undefined" && !rest.isOptional) {
-        throw new Error(`Required: ${key}`);
+        handleError(`Required: ${key}`);
       }
 
       if (typeof value !== "undefined") {
+        // @ts-ignore
         acc[key] = value;
       }
 
